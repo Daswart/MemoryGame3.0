fdd535bacb604e2f6c32f4008fbafa1e259d2cd5
Daswart <dswart@roc-dev.com>
2021-11-09 11:30:18 +0100

    made some changes

--- catMemoryGame3.0.js
+++ catMemoryGame3.0.js
@@ -1,17 +1,30 @@
 let movesMade = 0;
 let flippedCards = [];
 let flippedCardsArray = [];
+let aantalBeurten = 0;
 
 //putting DOM-elements in variables
 const playercontainer = document.getElementById('playercontainer')
+const refreshButton = document.getElementById('refreshbutton')
+const punten = document.getElementById('punten');
+console.log(punten)
 
+//knop om de pagina te refreshen.
+refreshButton.addEventListener('click', restartGame);
 
+//functie om het spel opnieuw te starten
+function restartGame() {
+    let r = confirm('Weet je zeker dat je het spel opnieuw wilt starten?');
+    if (r == true) {
+        location.reload();
+    } else {
+        console.log('pagina niet herladen!');
+    }
+}
 
-//
 
 
 let catArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
-
 //function to shuffle deck
 function shuffleDeck(array) {
     for (let i = 0; i < array.length; i++) {
@@ -37,6 +50,10 @@ function createImageholders() {
         imageHolder.id = i;
         imageHolder.className = 'item';
         imageHolder.style.width = '150px';
+        imageHolder.style.border = 'none';
+        imageHolder.style.borderRadius = '10px';
+        imageHolder.style.boxShadow = '5px 8px 6px #888888';
+        imageHolder.style.cursor = 'pointer';
         imageHolder.addEventListener("click", reactToCardClick)
         imageHolder.style.backgroundImage = 'url("img/cartooncat.png")'
         playercontainer.appendChild(imageHolder)
@@ -65,9 +82,12 @@ function reactToCardClick() {
 
 
     if (movesMade === 2) {
-        let allItems = document.getElementsByClassName('item');
         checkForMatch();
         movesMade = 0;
+        setTimeout(function () {
+            aantalBeurten++
+            punten.innerHTML = aantalBeurten;
+        }, 1000)
 
     }
 
@@ -90,9 +110,7 @@ function flipcard(id) {
 function checkForMatch() {
 
     let allItems = document.getElementsByClassName('item');
-    for (i = 0; i < allItems.length; i++) {
-        allItems[i].removeEventListener('click', reactToCardClick);
-    }
+
     //wanneer de kaarten gelijk aan elkaar zijn
     if (flippedCards[0] === flippedCards[1]) {
 
@@ -101,6 +119,7 @@ function checkForMatch() {
                 if (catArray[i] === flippedCards[0]) {
                     allItems[i].style.background = "none";
                     allItems[i].style.border = "none";
+                    allItems[i].style.boxShadow = "none";
                     allItems[i].removeEventListener('click', reactToCardClick);
                     catArray[i] = null;
                     console.log(catArray);
@@ -114,13 +133,17 @@ function checkForMatch() {
             flippedCardsArray.push(flippedCards[1]);
 
             console.log(flippedCardsArray);
-            console.log(allItems);
+            // console.log(allItems);
 
-        }, 1500);
 
-        for (i = 0; i < allItems.length; i++) {
-            allItems[i].addEventListener('click', reactToCardClick);
-        }
+
+            for (i = 0; i < allItems.length; i++) {
+                if (catArray[i] != null) {
+                    allItems[i].addEventListener('click', reactToCardClick);
+                }
+            }
+
+        }, 1000);
 
     } //Wanneer de plaatjes niet gelijk zijn. 
     else {
@@ -137,12 +160,17 @@ function checkForMatch() {
                     allItems[i].style.background = 'url("img/cartooncat.png")';
                 }
 
-                allItems[i].addEventListener('click', reactToCardClick)
-
+                if (catArray[i] != null) {
+                    allItems[i].addEventListener('click', reactToCardClick);
+                }
 
             }
         }, 1000);
 
     }
+    for (i = 0; i < allItems.length; i++) {
+        allItems[i].removeEventListener('click', reactToCardClick);
+    }
+
 
 }
